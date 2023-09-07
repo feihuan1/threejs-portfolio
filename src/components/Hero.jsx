492bec004ad8edcd28d31253da71be66906c738c
eric <you@example.comericrealtor1997@gmail.com>
2023-09-07 03:19:21 -0700

    hero section added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {motion} from 'framer-motion';
+import { ComputersCanvas } from './canvas';
+import { styles } from '../styles';
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+        <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+        <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Feihuan</span></h1>
+          <p className={`${styles.heroSubText} text-white-100 mt-2`}>
+            I love web development, 3D visuals, user <br className='sm:block hidden' /> interfaces and game development
+          </p>
+        </div>
+      </div>
+        <ComputersCanvas />
+    </section>
   )
 }
 
