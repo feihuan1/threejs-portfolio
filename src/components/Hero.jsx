56a90c2e352cd4233bf628b10bb53690a4549f1b
eric <you@example.comericrealtor1997@gmail.com>
2023-09-08 01:14:17 -0700

    welcome section set up

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import {motion} from 'framer-motion';
+
 import { ComputersCanvas } from './canvas';
 import { styles } from '../styles';
 
@@ -19,6 +20,24 @@ const Hero = () => {
         </div>
       </div>
         <ComputersCanvas />
+
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href="#about">
+            <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-self-center items-start p-2'>
+              <motion.dev 
+              animate={{
+                y:[0,24,0]
+              }} 
+              transition={{
+                duration:1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }} 
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+               />
+            </div>
+          </a>
+        </div>
     </section>
   )
 }
